64d9bafcc67449b9d446eecb40cb0fb148768fec
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-09-04 01:08:45 +0300

    Update AssemblyInfo.cs

--- EasyCSharpEncryptor/Properties/AssemblyInfo.cs
+++ EasyCSharpEncryptor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
 [assembly: AssemblyTitle("EasyCSharpEncryptor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Easy password generator & text encryptor of any length using password and salt (AES Encryption + SHA256)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SP1K3")]
 [assembly: AssemblyProduct("EasyCSharpEncryptor")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
